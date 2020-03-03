7b6453402699547f16a41dd7b127277de8fdad2a
MichaelNegrete16 <michael.negrete16@gmail.com>
2020-03-03 12:23:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto2/FifthFragment.java
@@ -0,0 +1,40 @@
+package com.example.proyecto2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+public class FifthFragment extends Fragment{
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agregar, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.saved2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FifthFragment.this)
+                        .navigate(R.id.action_fifthFragment_to_SecondFragment);
+            }
+        });
+
+        view.findViewById(R.id.regresar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FifthFragment.this)
+                        .navigate(R.id.action_fifthFragment_to_SecondFragment);
+            }
+        });
+    }
+}
